05f83e2fe6791f4241e0d030d6ad653953d1ac55
wangsa19 <dewanggawpw@gmail.com>
2024-04-25 20:52:45 +0700

    finish

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+
+app.get("/getUsers", function (req, res) {
+  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
+    console.log(data);
+    res.end(data);
+  });
+});
+
+var server = app.listen(8080, function () {
+  var port = server.address().port;
+  console.log("REST API demo app listening at http://localhost:%s", port);
+});
